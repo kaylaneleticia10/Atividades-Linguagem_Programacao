35e96ef5357708244c97142392c6294498f75637
kaylaneleticia10 <kaylaneleticia10@gmail.com>
2025-03-30 20:51:31 -0300

    atualizacao

--- Atividade 03- Botao Clique/script.js
+++ Atividade 03- Botao Clique/script.js
@@ -1,10 +1,18 @@
-botao = document.getElementById("botao");
+botao = document.getElementById("botao"); //captura o botao pelo id
 
-botao.onmouseover = function(){
-    botao.style.left=Math.random()*(window.innerWidth - botao.offsetWidth) +"px";
+botao.onmouseover = function(){ //quando o mouse passar pelo botao chama a funcao:
+    botao.style.left=Math.random()*(window.innerWidth - botao.offsetWidth) +"px"; 
+    //botao.style.left: define a posição horizontal do botão em relação a janela do navegador
+    //Math.random(): Gera um número aleatório 
+    //window.innerWidth: pega o valor da largura da janela do navegador
+    //botao.offsetWidth: subtrai a largura do botao para que ele nao fique cortado
+    //+ "px" garante que esse número seja interpretado como pixels pelo CSS
     botao.style.top=Math.random()*(window.innerHeight - botao.offsetHeight) +"px";
+    //botao.style.top: define a posição vertical do botão em relação ao topo da janela do navegador
+    //Math.random(): Gera um número aleatório 
+    //window.innerHeight: pega o valor da altura da janela do navegador
+    //botao.offsetWidth: subtrai a largura do botao para que ele nao fique cortado
 };
-
-botao.onfocus = function() {
-    botao.blur();
+botao.onfocus = function() { //quando o botao tentar ser focado
+    botao.blur(); //chama a funcao para desfocar o botao
 };
